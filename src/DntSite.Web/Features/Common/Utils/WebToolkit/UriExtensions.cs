71578d5ce42aebb82c106ca5dc7eff95fb9f5f6c
VahidN <vahid_nasiri@yahoo.com>
2024-07-29 15:20:08 +0330

    Update FromFeedKey

--- src/DntSite.Web/Features/Common/Utils/WebToolkit/UriExtensions.cs
+++ src/DntSite.Web/Features/Common/Utils/WebToolkit/UriExtensions.cs
@@ -4,7 +4,7 @@ namespace DntSite.Web.Features.Common.Utils.WebToolkit;
 
 public static class UriExtensions
 {
-    public const string FromFeedKey = "updated";
+    public const string FromFeedKey = "utm_source";
 
     public static bool IsFromFeed(this NavigationManager navigationManager)
     {
