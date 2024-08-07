151d3d4cb758f08c01791027e0f9b9328f8785c9
VahidN <vahid_nasiri@yahoo.com>
2024-08-07 13:03:29 +0330

    Add sitemap.xml routing

--- src/DntSite.Web/Features/Seo/Controllers/SitemapController.cs
+++ src/DntSite.Web/Features/Seo/Controllers/SitemapController.cs
@@ -1,16 +1,21 @@
 using DntSite.Web.Features.Posts.Entities;
 using DntSite.Web.Features.Posts.Services.Contracts;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace DntSite.Web.Features.Seo.Controllers;
 
 [ApiController]
 [AllowAnonymous]
-[Microsoft.AspNetCore.Mvc.Route("[controller]")]
+[Microsoft.AspNetCore.Mvc.Route(template: "[controller]")]
 public class SitemapController(IBlogPostsService blogPostsService) : ControllerBase
 {
+    [OutputCache(Duration = TimeConstants.Minute * 15, PolicyName = AlwaysCachePolicy.Name)]
+    [Microsoft.AspNetCore.Mvc.Route(template: "/sitemap.xml")]
+    [Microsoft.AspNetCore.Mvc.Route(template: "/sitemap")]
+    [Microsoft.AspNetCore.Mvc.Route(template: "[action]")]
     public async Task<IActionResult> Get()
     {
-        var items = await blogPostsService.GetLastBlogPostsIncludeAuthorTagsAsync(15);
+        var items = await blogPostsService.GetLastBlogPostsIncludeAuthorTagsAsync(count: 15);
 
         var sitemapItems = items.Where(item => !IsPrivate(item))
             .Select(item => new SitemapItem
