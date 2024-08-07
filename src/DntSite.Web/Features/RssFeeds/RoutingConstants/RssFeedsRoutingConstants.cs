6f18458bfc0864fc0874dda71a4ef02f74efec4a
VahidN <vahid_nasiri@yahoo.com>
2024-08-07 11:44:09 +0330

    Add old SiteLogs routing

--- src/DntSite.Web/Features/RssFeeds/RoutingConstants/RssFeedsRoutingConstants.cs
+++ src/DntSite.Web/Features/RssFeeds/RoutingConstants/RssFeedsRoutingConstants.cs
@@ -3,4 +3,5 @@ namespace DntSite.Web.Features.RssFeeds.RoutingConstants;
 public static class RssFeedsRoutingConstants
 {
     public const string WhatsNew = "/whats-new";
+    public const string SiteLogs = "/SiteLogs";
 }
