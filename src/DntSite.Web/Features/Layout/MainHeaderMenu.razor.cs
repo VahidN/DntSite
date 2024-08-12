9758e22c95f275e0e742423505ede86b059326c1
VahidN <vahid_nasiri@yahoo.com>
2024-08-12 14:13:26 +0330

    Return to last page after login, Close #11

--- src/DntSite.Web/Features/Layout/MainHeaderMenu.razor.cs
+++ src/DntSite.Web/Features/Layout/MainHeaderMenu.razor.cs
@@ -1,4 +1,5 @@
 using DntSite.Web.Features.AppConfigs.Components;
+using DntSite.Web.Features.UserProfiles.RoutingConstants;
 
 namespace DntSite.Web.Features.Layout;
 
@@ -7,4 +8,7 @@ public partial class MainHeaderMenu
     [CascadingParameter] internal ApplicationState ApplicationState { set; get; } = null!;
 
     private string? SiteName => ApplicationState.AppSetting?.BlogName;
+
+    private string LoginUrl
+        => $"{UserProfilesRoutingConstants.Login}?returnUrl={Uri.EscapeDataString(ApplicationState.CurrentAbsoluteUri.ToString())}";
 }
