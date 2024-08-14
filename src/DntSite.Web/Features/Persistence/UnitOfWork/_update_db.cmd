6cf0e9bf284cc532c0ddbe41b43e15ffad1bd787
VahidN <vahid_nasiri@yahoo.com>
2024-08-14 11:36:36 +0330

    Upgrade to .NET 8.0.400

--- src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 8.0.7
+dotnet tool update --global dotnet-ef --version 8.0.8
 dotnet tool restore
 dotnet ef --verbose --project ../../../DntSite.Web.csproj --startup-project ../../../ database update
 pause
