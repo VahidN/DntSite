4ca75f93a0f79f3684903ec3ec2338acb79f3744
VahidN <vahid_nasiri@yahoo.com>
2024-09-30 15:22:32 +0330

    Add DntCardAccordion

--- src/DntSite.Web.Common.BlazorSsr/wwwroot/scripts/app.js
+++ src/DntSite.Web.Common.BlazorSsr/wwwroot/scripts/app.js
@@ -1,4 +1,35 @@
-﻿window.DntStickySidebar = {
+﻿window.DntCardAccordion = {
+    enable: () => {
+        document.querySelectorAll("button[data-dnt-collapse]").forEach(element => {
+            element.title = "بستن محتوا";
+            element.onclick = () => {
+                const card = element.parentElement?.parentElement;
+                if (!card) {
+                    return;
+                }
+
+                card.querySelectorAll(".card-body, .card-footer, .list-group").forEach(cardChild => {
+                    if (cardChild.classList.contains("collapse")) {
+                        cardChild.classList.remove("accordion-collapse", "collapse");
+
+                        element.firstElementChild.classList.remove("bi-chevron-left");
+                        element.firstElementChild.classList.add("bi-chevron-down");
+                        element.title = "بستن محتوا";
+
+                    } else {
+                        cardChild.classList.add("accordion-collapse", "collapse");
+
+                        element.firstElementChild.classList.remove("bi-chevron-down");
+                        element.firstElementChild.classList.add("bi-chevron-left");
+                        element.title = "گشودن محتوا";
+                    }
+                });
+            };
+        });
+    }
+};
+
+window.DntStickySidebar = {
     enable: () => {
         const sideBarMenu = document.querySelector("div#sidebar-menu > ul");
         if (sideBarMenu) {
@@ -1637,6 +1668,7 @@ window.DntAddActiveClassToLists = {
 
 window.DntUtilities = {
     enable: () => {
+        DntCardAccordion.enable();
         DntHljsCopyToClipboardPlugin.enable();
         DntAddActiveClassToLists.enable();
         DntPersianDatePicker.enable();
