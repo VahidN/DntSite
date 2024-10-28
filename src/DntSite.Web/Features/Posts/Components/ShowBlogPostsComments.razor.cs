4577c968927f121e6cb0905ce956a0227d95dfe9
VahidN <vahid_nasiri@yahoo.com>
2024-10-28 14:09:22 +0330

    Improve page's title of ShowBlogPostsComments

--- src/DntSite.Web/Features/Posts/Components/ShowBlogPostsComments.razor.cs
+++ src/DntSite.Web/Features/Posts/Components/ShowBlogPostsComments.razor.cs
@@ -20,7 +20,8 @@ public partial class ShowBlogPostsComments
 
     private string MainTitle => !HasUserFriendlyName ? MainPageTitle : MainUserPageTitle;
 
-    private string MainUserPageTitle => $@"آرشیو نظرات مطالب {UserFriendlyName}";
+    private string MainUserPageTitle => string.Create(CultureInfo.InvariantCulture,
+        $"آرشیو نظرات مطالب {UserFriendlyName}، صفحه: {CurrentPage ?? 1}");
 
     private string MainUserPageUrl
         => !HasUserFriendlyName ? MainPageUrl : $"{MainPageUrl}/{Uri.EscapeDataString(UserFriendlyName)}";
