73fbefa76c469a5c0de3ec799bb7ae3356240e3f
VahidN <vahid_nasiri@yahoo.com>
2024-11-11 13:37:12 +0330

    Add top and bottom margin to sites images

--- src/DntSite.Web.Common.BlazorSsr/Scripts/dntStyleSiteImages.ts
+++ src/DntSite.Web.Common.BlazorSsr/Scripts/dntStyleSiteImages.ts
@@ -2,8 +2,16 @@ namespace DntBlazorSsr {
     export class DntStyleSiteImages {
         static enable(): void {
             document.querySelectorAll<HTMLImageElement>('img').forEach(el => {
-                if (el.src && el.src.toLowerCase().includes("/file/")) {
+                if (!el.src) {
+                    return;
+                }
+
+                const src = el.src.toLowerCase();
+                if (src.includes("/file/")) {
                     el.classList.add("rounded", "border", "shadow-sm", "border-secondary-subtle");
+                    if (!src.includes("avatar")) {
+                        el.classList.add("mt-2", "mb-2");
+                    }
                 }
             });
         }
