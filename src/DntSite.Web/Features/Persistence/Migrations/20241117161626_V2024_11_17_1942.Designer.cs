93d2d27281207f98f482aa5d9a38749d5f25547c
VahidN <vahid_nasiri@yahoo.com>
2024-11-17 19:58:32 +0330

    Update migrations

--- src/DntSite.Web/Features/Persistence/Migrations/20241117161626_V2024_11_17_1942.Designer.cs
+++ src/DntSite.Web/Features/Persistence/Migrations/20241117161626_V2024_11_17_1942.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DntSite.Web.Features.Persistence.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DntSite.Web.Features.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241117161626_V2024_11_17_1942")]
+    partial class V2024_11_17_1942
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DntSite.Web/Features/Persistence/Migrations/20241117161626_V2024_11_17_1942.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DntSite.Web.Features.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2024_11_17_1942 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
