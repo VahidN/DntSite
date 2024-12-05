6f1c8515e0fcded63d0994ed94c2004ac8453623
VahidN <vahid_nasiri@yahoo.com>
2024-12-05 20:33:54 +0330

    Show news links on what's new page

--- src/DntSite.Web/Features/News/Components/PrintNewsRedirect.razor.cs
+++ src/DntSite.Web/Features/News/Components/PrintNewsRedirect.razor.cs
@@ -1,21 +1,25 @@
-using DntSite.Web.Features.News.Entities;
 using DntSite.Web.Features.News.RoutingConstants;
 
 namespace DntSite.Web.Features.News.Components;
 
 public partial class PrintNewsRedirect
 {
-    [Parameter] [EditorRequired] public DailyNewsItem? DailyNewsItem { set; get; }
+    [Parameter] [EditorRequired] public int Id { get; set; }
 
-    private string StatusIcon => DailyNewsItem?.LastHttpStatusCode is HttpStatusCode.OK ? "bg-success" : "bg-warning";
+    [Parameter] [EditorRequired] public HttpStatusCode? LastHttpStatusCode { get; set; }
 
-    private string HttpStatusCodeText => DailyNewsItem?.LastHttpStatusCode is null
+    [Parameter] [EditorRequired] public DateTime? LastHttpStatusCodeCheckDateTime { get; set; }
+
+    [Parameter] [EditorRequired] public string? Url { get; set; }
+
+    private string StatusIcon => LastHttpStatusCode is HttpStatusCode.OK ? "bg-success" : "bg-warning";
+
+    private string HttpStatusCodeText => LastHttpStatusCode is null
         ? "نامشخص"
-        : string.Create(CultureInfo.InvariantCulture,
-            $"{(int)DailyNewsItem.LastHttpStatusCode.Value}, {DailyNewsItem.LastHttpStatusCode.Value}");
+        : string.Create(CultureInfo.InvariantCulture, $"{(int)LastHttpStatusCode.Value}, {LastHttpStatusCode.Value}");
 
-    private string UrlHost => DailyNewsItem is not null ? new Uri(DailyNewsItem.Url).Host : "";
+    private string UrlHost => Url.IsValidUrl() ? new Uri(Url).Host : "";
 
-    private string GetUrl(DailyNewsItem dailyNewsItem)
-        => string.Create(CultureInfo.InvariantCulture, $"{NewsRoutingConstants.NewsRedirectBase}/{dailyNewsItem.Id}");
+    private string RedirectUrl
+        => string.Create(CultureInfo.InvariantCulture, $"{NewsRoutingConstants.NewsRedirectBase}/{Id}");
 }
