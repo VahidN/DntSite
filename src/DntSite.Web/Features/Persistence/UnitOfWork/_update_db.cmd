f0817ff30ac2e9754751eaa99dbed1731c025c6f
VahidN <vahid_nasiri@yahoo.com>
2025-01-15 21:30:25 +0330

    Upgrade dependencies to .NET 9.0.102

--- src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 9.0.0
+dotnet tool update --global dotnet-ef --version 9.0.1
 dotnet tool restore
 dotnet ef --verbose --project ../../../DntSite.Web.csproj --startup-project ../../../ database update
 pause
