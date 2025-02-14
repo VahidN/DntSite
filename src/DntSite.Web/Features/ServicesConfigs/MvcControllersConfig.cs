e2dbc797d52ebc554da6e8953fc3bd17ff6834db
VahidN <vahid_nasiri@yahoo.com>
2025-02-14 13:59:39 +0330

    Update dependencies

--- src/DntSite.Web/Features/ServicesConfigs/MvcControllersConfig.cs
+++ src/DntSite.Web/Features/ServicesConfigs/MvcControllersConfig.cs
@@ -1,25 +1,9 @@
-using System.Text.Encodings.Web;
-using System.Text.Unicode;
 using Microsoft.AspNetCore.Http.Timeouts;
 
 namespace DntSite.Web.Features.ServicesConfigs;
 
 public static class MvcControllersConfig
 {
-    public static readonly JsonSerializerOptions JsonSerializerOptions = new()
-    {
-        NumberHandling = JsonNumberHandling.AllowReadingFromString,
-        PropertyNameCaseInsensitive = true,
-        WriteIndented = true,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        Converters =
-        {
-            new JsonStringEnumConverter()
-        },
-        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
-        ReferenceHandler = ReferenceHandler.IgnoreCycles
-    };
-
     public static IMvcBuilder AddCustomizedControllers(this IServiceCollection services)
         => services.AddProblemDetails()
             .Configure<RouteOptions>(opt =>
@@ -37,18 +21,5 @@ public static class MvcControllersConfig
             .AddLargeFilesUploadSupport()
             .AddOutputCache(options => { options.AddPolicy(AlwaysCachePolicy.Name, AlwaysCachePolicy.Instance); })
             .AddControllers(options => { options.Filters.Add<ApplyCorrectYeKeFilterAttribute>(); })
-            .AddCustomJsonOptions();
-
-    private static IMvcBuilder AddCustomJsonOptions(this IMvcBuilder builder)
-        => builder.AddJsonOptions(options =>
-        {
-            var jsonSerializerOptions = options.JsonSerializerOptions;
-            jsonSerializerOptions.NumberHandling = JsonSerializerOptions.NumberHandling;
-            jsonSerializerOptions.PropertyNameCaseInsensitive = JsonSerializerOptions.PropertyNameCaseInsensitive;
-            jsonSerializerOptions.WriteIndented = JsonSerializerOptions.WriteIndented;
-            jsonSerializerOptions.DefaultIgnoreCondition = JsonSerializerOptions.DefaultIgnoreCondition;
-            jsonSerializerOptions.Converters.AddRange(JsonSerializerOptions.Converters);
-            jsonSerializerOptions.Encoder = JsonSerializerOptions.Encoder;
-            jsonSerializerOptions.ReferenceHandler = JsonSerializerOptions.ReferenceHandler;
-        });
+            .AddCustomJsonOptionsForWebApps();
 }
