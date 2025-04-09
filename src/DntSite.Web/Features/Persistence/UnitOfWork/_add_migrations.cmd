2664cc5fe8d0e559602b3b7826e4607316dcfbb9
VahidN <vahid_nasiri@yahoo.com>
2025-04-09 20:07:58 +0330

    Update dependencies

--- src/DntSite.Web/Features/Persistence/UnitOfWork/_add_migrations.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_add_migrations.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 9.0.3
+dotnet tool update --global dotnet-ef --version 9.0.4
 dotnet tool restore
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME: =0%") do (set mytime=%%a%%b)
