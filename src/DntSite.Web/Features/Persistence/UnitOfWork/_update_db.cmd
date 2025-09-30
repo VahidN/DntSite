6e610ef58657e3823e9c5551c4d25dc4c46487d3
VahidN <vahid_nasiri@yahoo.com>
2025-09-30 14:29:25 +0330

    Update dependencies

--- src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 9.0.8
+dotnet tool update --global dotnet-ef --version 9.0.9
 dotnet tool restore
 dotnet ef --verbose --project ../../../DntSite.Web.csproj --startup-project ../../../ database update
 pause
