abfec9eccf963d30befdf54d835171426999031c
VahidN <vahid_nasiri@yahoo.com>
2026-01-23 21:02:56 +0330

    Update dependencies

--- src/DntSite.Web/Features/Persistence/UnitOfWork/_add_migrations.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_add_migrations.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 10.0.1
+dotnet tool update --global dotnet-ef --version 10.0.2
 dotnet tool restore
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME: =0%") do (set mytime=%%a%%b)
--- src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
+++ src/DntSite.Web/Features/Persistence/UnitOfWork/_update_db.cmd
@@ -1,4 +1,4 @@
-dotnet tool update --global dotnet-ef --version 10.0.1
+dotnet tool update --global dotnet-ef --version 10.0.2
 dotnet tool restore
 dotnet ef --verbose --project ../../../DntSite.Web.csproj --startup-project ../../../ database update
 pause
