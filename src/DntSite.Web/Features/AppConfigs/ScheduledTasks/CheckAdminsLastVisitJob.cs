c12d1279c7f799805fc7056e7bb86138b40e8a67
VahidN <vahid_nasiri@yahoo.com>
2026-01-23 21:08:30 +0330

    Add a job to check if admin is present

---
+++ src/DntSite.Web/Features/AppConfigs/ScheduledTasks/CheckAdminsLastVisitJob.cs
@@ -0,0 +1,20 @@
+﻿using DntSite.Web.Features.AppConfigs.Services.Contracts;
+using DntSite.Web.Features.UserProfiles.Services.Contracts;
+
+namespace DntSite.Web.Features.AppConfigs.ScheduledTasks;
+
+public class CheckAdminsLastVisitJob(IUsersInfoService usersInfoService, IAppSettingsService appSettingsService)
+    : IScheduledTask
+{
+    public async Task RunAsync(CancellationToken cancellationToken)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return;
+        }
+
+        var limit = DateTime.UtcNow.AddDays(value: -2);
+        var siteIsActive = await usersInfoService.IsAnyActiveAdminPresentAsync(limit);
+        await appSettingsService.ChangeSiteActiveStateAsync(siteIsActive);
+    }
+}
