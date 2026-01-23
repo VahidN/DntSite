6ae15bc70e3205fbd6ed1a84e7bb97ebd7fd6a47
VahidN <vahid_nasiri@yahoo.com>
2026-01-23 21:04:06 +0330

    Improve online/offline check

--- src/DntSite.Web.Common.BlazorSsr/Scripts/dntStatusCheck.ts
+++ src/DntSite.Web.Common.BlazorSsr/Scripts/dntStatusCheck.ts
@@ -3,14 +3,31 @@
         static updateOnlineStatus(): void {
             document.querySelectorAll<HTMLInputElement>('[id^="dnt-online-status-report"]').forEach(statusBarElement => {
                 if (navigator.onLine) {
-                    statusBarElement.classList.add('d-none');
-                    statusBarElement.innerHTML = '';
+                    DntStatusCheck.hideStatusBarElement(statusBarElement);
                 } else {
-                    statusBarElement.classList.remove('d-none');
-                    statusBarElement.classList.add('badge', 'text-bg-danger', 'fs-6', 'mt-1', 'mb-2', 'show');
-                    statusBarElement.innerHTML = 'مرورگر در این لحظه آنلاین نیست. لطفا از اتصال خود به شبکه مطمئن شوید.';
+                    if (DntStatusCheck.isLocalEnvironment()) {
+                        DntStatusCheck.hideStatusBarElement(statusBarElement);
+                        return;
+                    }
+
+                    DntStatusCheck.onlineCheck("/").then(() => {
+                        DntStatusCheck.hideStatusBarElement(statusBarElement);
+                    }).catch(() => {
+                        DntStatusCheck.showStatusBarElement(statusBarElement);
+                    });
                 }
             });
+        }
+
+        static showStatusBarElement(statusBarElement: HTMLInputElement): void {
+            statusBarElement.classList.remove('d-none');
+            statusBarElement.classList.add('badge', 'text-bg-danger', 'fs-6', 'mt-1', 'mb-2', 'show');
+            statusBarElement.innerHTML = 'مرورگر در این لحظه آنلاین نیست. لطفا از اتصال خود به شبکه مطمئن شوید.';
+        }
+
+        static hideStatusBarElement(statusBarElement: HTMLInputElement): void {
+            statusBarElement.classList.add('d-none');
+            statusBarElement.innerHTML = '';
         }
 
         static enable(): void {
@@ -19,6 +36,29 @@
 
             // Initial check
             DntStatusCheck.updateOnlineStatus();
+        }
+
+        static onlineCheck(baseUrl: string): Promise<boolean> {
+            let xhr = new XMLHttpRequest();
+            return new Promise((resolve, reject) => {
+                xhr.onload = () => {
+                    resolve(true);
+                };
+                xhr.onerror = () => {
+                    reject(false);
+                };
+                xhr.open('GET', baseUrl, true);
+                xhr.send();
+            });
+        }
+
+        static isLocalEnvironment(): boolean {
+            const {protocol, hostname} = window.location;
+
+            const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1' || hostname === '[::1]';
+            const isPrivateIP = /^10\./.test(hostname) || /^192\.168\./.test(hostname) || /^172\.(1[6-9]|2\d|3[0-1])\./.test(hostname);
+
+            return protocol === 'file:' || isLocalhost || isPrivateIP;
         }
     }
 }
